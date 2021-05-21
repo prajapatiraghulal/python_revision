8e264863342477585ee02875cc71bd391bfc2e18
Raghulal Prajapati <56857544+prajapatiraghulal@users.noreply.github.com>
2021-05-21 19:54:40 +0530

    oops_intro.py file added

---
+++ oops_intro.py
@@ -0,0 +1,34 @@
+class Laptop:
+    disc=10
+    def __init__(self,name,price):
+        #instance variables
+        self.name = name
+        self.price = price
+    
+    def discount(self):
+        return (self.price-self.price/100.0)*self.disc      #self.disc is for instance only and can be used for all and for specific variable also
+
+l1= Laptop('asus z',32000)
+l2= Laptop('hp',70000)
+
+print(l1.name,',',l2.name)
+# # # # # # # # print(Laptop.__init__(l1,'ram','shyam'),l1.name)  #inside calling of instance function
+print(l1.name)
+
+print(l1.discount(), l2.discount())
+print(Laptop.discount(l1), Laptop.discount(l2))  #this is similar to just above code this is actually done by interpreter internally 
+
+# l=[1,2,3]
+# list.clear(l)
+# print(l)
+
+print(l1.__dict__, l2.__dict__)
+
+l3= Laptop('dell', 65000)
+l3.disc = 20
+print(l3.__dict__, l3.discount())
+
+
+###we can define specific instance variable which is not defined 
+# l2.new_name = 'dell new'
+# print(l2.__dict__)
